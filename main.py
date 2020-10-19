bc0050bdd937d2d3f4a6af977d478ba7b4741601
Svetlanamsv <svetikmsv34@gmail.com>
2020-10-19 19:55:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Enter two numbers: ", end="\n")
+a=int(input())
+b=int(input())
+
+if a>b:
+    print("The first number is greater than the second")
+else:
+    print("The second number is greater than the first")
